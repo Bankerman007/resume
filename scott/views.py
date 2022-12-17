297d988cc90026e7bafc1340caa27dedf0f8252a
Scott Wright <scottwright.qrf@gmail.com>
2022-12-16 18:22:34 -0600

    initial commit

---
+++ scott/views.py
@@ -0,0 +1,31 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def base(request):
+    return render(request, 'base.html',{})
+    #return HttpResponse("Hello, world. Scott is hot.")
+
+def trade_bot(request):
+    return render(request, 'trade_bot.html',{})
+
+def bg_nav(request):
+    return render(request, 'bg_nav.html',{})
+
+def certif(request):
+    return render(request, 'certif.html',{})
+
+def web_react(request):
+    return render(request, 'web_react.html',{})
+
+def roster(request):
+    return render(request, 'roster.html',{})
+
+def team_creator(request):
+    return render(request, 'team_creator.html',{})
+
+def prof_exp(request):
+    return render(request, 'prof_exp.html',{})
+
+
+
