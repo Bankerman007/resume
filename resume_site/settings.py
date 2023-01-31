a7eb54a7235284987935f2cc9b8e4f15e46f089f
Scott Wright <scottwright.qrf@gmail.com>
2023-01-30 19:24:01 -0600

    changes

--- resume_site/settings.py
+++ resume_site/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+rpr=8@h!eoi1tfwo#s!-^wf!-tuumqs+5wnlvc)3y+11@ow_='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1","scott-wright.justdev.us"]
 
 
 # Application definition
@@ -122,3 +122,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+TICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
